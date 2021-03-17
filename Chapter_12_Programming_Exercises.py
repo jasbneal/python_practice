d9e78d1568adc00106147854b186caedf60c9d4b
Jasmine Neal <75139330+jasbneal@users.noreply.github.com>
2021-03-17 11:23:56 -0400

    Add files via upload
    Starting Out With Python CHPT 12 (Recursion) Programming Exercises #2 & #3

--- Chapter_12_Programming_Exercises.py
+++ Chapter_12_Programming_Exercises.py
@@ -17,3 +17,49 @@ def show_num(n):
         print(n)
 
 show_num(n)
+
+# 16 March 2021
+# Starting Out With Python Exercise #2
+# Design a recursive function that accepts two arguments into the parameters x and y.
+# The function whould return the value of x times y. Remember, the multiplication can be
+# performed as repeated additions as follows:
+# 7 X 4 = 4 + 4 + 4 + 4 + 4 + 4 + 4
+# (To keep the function simple, assume that x and y will always hold positive nonzero integers.)
+
+try:
+    x = int(input("Enter any positive nonzero integer for x: "))
+    y = int(input("Enter any positive nonzero integer for y: "))
+except ValueError:
+    print("ERROR: You must enter a positive nonzero integer. Try again.")
+    x = int(input("Enter any positive nonzero integer for x: "))
+    y = int(input("Enter any positive nonzero integer for y: "))
+
+def multiply_nums(x, y):
+    if x == 1:
+        return y
+    if x > 1:
+        return y + multiply_nums(x - 1, y)
+
+total = multiply_nums(x, y)
+print(total)
+
+# 16 March 2021
+# Starting Out With Python Exercise #3
+# Write a recursive function that accepts an argument, n. The function should display n
+# lines of asterisks on the screen, with the first line showing 1 asterisk, the second line
+# showing 2 asterisks, up to the nth line which shows n asterisks.
+
+try:
+    n = int(input("Enter any whole number: "))
+except ValueError:
+    print("ERROR: You must enter a whole number. Try again.")
+    n = int(input("Enter any whole number: "))
+
+def asterisks(n):
+    if n == 1:
+        print('*')
+    if n > 1:
+        asterisks(n - 1) 
+        print('*' * n)
+
+asterisks(n)
